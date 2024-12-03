b496d3e1cdabb8911c0deed2d9ed981e49f32373
rons003 <ronelle003@gmail.com>
2024-12-04 02:58:55 +0800

    update get establish

--- controllers/resident_controller.py
+++ controllers/resident_controller.py
@@ -54,43 +54,39 @@ def index():
 
 def show(id):
     try:
-        sql = """
-            SELECT
-                a.id as resident_id,
-                a.*,
-                b.*
-            FROM
-                resident a
-            INNER JOIN
-                establishment b
-            ON
-                b.id = a.establishment_id
-            WHERE
-                a.id = :id"""
-        resident = db.session.execute(text(sql), {"id": id}).first()
-        if resident is None:
+        establishment = db.session.query(
+            Establishment).filter_by(id=id).first()
+
+        if establishment is None:
             return make_response(
-                jsonify({'message': 'Invalid Resident ID'}), 400)
+                jsonify({'message': 'Invalid Establishment ID'}), 400)
+        residents = []
+        for resident in establishment.resident:
+            residents.append({
+                "id": resident.id,
+                "first_name": resident.first_name,
+                "middle_name": resident.middle_name,
+                "last_name": resident.last_name,
+                "occupation": resident.occupation,
+                "present_address": resident.present_address,
+                "age": resident.age,
+                "gender": resident.sex,
+                "nationality": resident.nationality,
+                "civil_status": resident.civil_status,
+                "birth_date": resident.birth_date,
+                "contact_no": resident.contact_no,
+                "emergency_name": resident.emergency_name,
+                "emergency_contact_no": resident.emergency_contact_no
+            })
+        images = []
         result = {
-            "id": resident.resident_id,
-            "establishment_id": resident.establishment_id,
-            "first_name": resident.first_name,
-            "middle_name": resident.middle_name,
-            "last_name": resident.last_name,
-            "occupation": resident.occupation,
-            "present_address": resident.present_address,
-            "age": resident.age,
-            "sex": resident.sex,
-            "nationality": resident.nationality,
-            "civil_status": resident.civil_status,
-            "birth_date": resident.birth_date,
-            "contact_no": resident.contact_no,
-            "emergency_name": resident.emergency_name,
-            "emergency_contact_no": resident.emergency_contact_no,
-            "code": resident.code,
-            "block": resident.block,
-            "address": resident.address,
-            "type": resident.type
+            "id": establishment.id,
+            "code": establishment.code,
+            "block": establishment.block,
+            "address": establishment.address,
+            "type": establishment.type,
+            "residents": residents,
+            "images": images
         }
         return make_response(jsonify(result), 200)
     except Exception as e:
@@ -131,7 +127,7 @@ def create():
             filesnames = []
             for image in data['images']:
                 image = EstablishmentImage()
-                image.filename = time.strftime("%Y%m%d%H%M%S")+ ".jpg"
+                image.filename = time.strftime("%Y%m%d%H%M%S") + ".jpg"
                 filesnames.append(image)
             establishment.establishment_image = filesnames
 
@@ -200,4 +196,3 @@ def update(id):
 def convert_and_save(b64_string, name):
     with open("test.png", "wb") as fh:
         fh.write(base64.decodebytes(b64_string.encode('utf-8')))
-        
